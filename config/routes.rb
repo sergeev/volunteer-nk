034f8513ad19ae9a93d54d2d99162cbc15385253
sergeev <v.sergeev.m@gmail.com>
2018-07-14 20:35:58 +0530

    Create controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'events/index'
+  get 'results/index'
+  get 'partner/index'
+  get 'persons/index'
+  get 'volunteer/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
